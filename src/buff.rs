b5b17246b058fab30e97a713fab33d84d2579cb4
Martin Wernstål <m4rw3r@gmail.com>
2018-07-29 14:20:52 +0200

    Intensity stacking buffs

--- src/buff.rs
+++ src/buff.rs
@@ -6,6 +6,8 @@ use AgentId;
 use std::mem;
 
 pub type Quickness = Simulator<Duration<Queue, [u32; 5]>>;
+pub type Might     = Simulator<Intensity<Replace, [u32; 25]>>;
+pub type BasicBuff = Simulator<Duration<Replace, [u32; 1]>>;
 
 pub trait Stack {
     /// Creates a new empty stack.
@@ -66,7 +68,47 @@ pub struct Intensity<T: StackType, U: Sized>(T, U);
 
 macro_rules! impl_intensity {
     ($t:ident, $n:expr) => {
+impl Stack for Intensity<$t, [u32; $n]> {
+    fn new() -> Self {
+        Intensity($t, [0; $n])
+    }
 
+    fn push(&mut self, mut stack: u32) -> u32 {
+        for (i, s) in self.1.iter_mut().enumerate() {
+            // Add if the stack is empty or if we are above the locked items and it is lower
+            if $t::can_replace(i, *s, stack) {
+                mem::swap(&mut stack, s);
+            }
+
+            if stack == 0 {
+                return 0;
+            }
+        }
+
+        // Return the overstack
+        stack
+    }
+
+    fn update(&mut self, time: u32) {
+        for d in self.1.iter_mut() {
+            *d = d.saturating_sub(time);
+        }
+    }
+
+    fn sum(&self) -> u32 {
+        self.1.iter().filter(|&&i| i > 0).sum()
+    }
+
+    fn stacks(&self) -> usize {
+        self.1.iter().filter(|&&i| i > 0).count()
+    }
+
+    fn clear(&mut self) {
+        for d in self.1.iter_mut() {
+            *d = 0;
+        }
+    }
+}
     }
 }
 
@@ -107,7 +149,6 @@ impl Stack for Duration<$t, [u32; $n]> {
             // Wait with the sort until we have a zero
             if self.1[0] == 0 {
                 // Sort
-                // TODO: Is this sorting actually what happens?
                 for i in 0..($n - 1) {
                     if self.1[i] < self.1[i + 1] {
                         self.1.swap(i, i + 1);
@@ -142,18 +183,28 @@ impl Stack for Duration<$t, [u32; $n]> {
 
 impl_duration!(Replace, 1);
 impl_duration!(Queue, 5);
+impl_intensity!(Replace, 1);
 impl_intensity!(Replace, 25);
 impl_intensity!(Replace, 1500);
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct Simulator<T: Stack> {
+    /// The boon-stack.
+    #[serde(skip)]
     stack:     T,
+    /// Last timestamp observed, milliseconds.
+    #[serde(skip)]
     time:      u64,
+    /// The agent we are interested in, needed since we have to check either source or target
+    /// depending on if it is a cleanse or not.
+    #[serde(skip)]
+    agent:     AgentId,
     /// Sum of duration which got applied, milliseconds.
     uptime:    u32,
     /// Sum of extra duration which had no effect, milliseconds.
     overstack: u32,
-    agent:     AgentId,
+    /// Sum of duration stripped, milliseconds.
+    stripped:  u32,
 }
 
 impl<T: Stack> Simulator<T> {
@@ -164,6 +215,7 @@ impl<T: Stack> Simulator<T> {
             time:      0,
             uptime:    0,
             overstack: 0,
+            stripped:  0,
             agent,
         }
     }
@@ -182,22 +234,18 @@ impl<T: Stack> Simulator<T> {
         match e.removal() {
             // If we are not a buff-removal, the target receives the boon
             BuffRemoval::None   => if e.target_agent() == self.agent {
-                // self.overstack += cmp::max(0, e.overstack());
-
                 let over = self.stack.push(e.duration());
 
                 self.uptime    += e.duration().saturating_sub(over); // e.overstack());
                 self.overstack += over; // e.overstack();
-
-                println!("applied: {:>6}, overstacked: {:>6}, actual: {:>6}", e.duration(), e.overstack(), over);
-
             },
 
             // If we are a buff-removal, it is the source which has the boon removed
             BuffRemoval::All | BuffRemoval::Single | BuffRemoval::Manual => if e.agent() == self.agent {
-                println!("Lost {} ms", self.stack.sum());
+                let sum = self.stack.sum();
 
-                self.uptime = if self.stack.sum() > self.uptime { 0 } else { self.uptime - self.stack.sum() };
+                self.uptime    = if sum > self.uptime { 0 } else { self.uptime - sum };
+                self.stripped += sum;
 
                 self.stack.clear();
             },
@@ -222,6 +270,11 @@ impl<T: Stack> Simulator<T> {
     #[inline]
     pub fn overstack(&self) -> u32 {
         self.overstack
+    }
+
+    #[inline]
+    pub fn stripped(&self) -> u32 {
+        self.stripped
     }
 }
 
